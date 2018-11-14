d33ca9e6bd55a39324a17bec09b9381fa4e5bddf
Michael Jordan <mrmikeman38@gmail.com>
2018-11-14 14:12:29 +1100

    Actually added script

---
+++ script/devPassword.js
@@ -0,0 +1,19 @@
+function password() {
+var testV = 1;
+var pass1 = prompt('Please Enter Your Password',' ');
+while (testV < 3) {
+if (!pass1)
+history.go(-1);
+if (pass1.toLowerCase() == "letmein") {
+alert('You Got it Right!');
+window.open('../pages/dev.html');
+break;
+}
+testV+=1;
+var pass1 =
+prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+}
+if (pass1.toLowerCase()!="password" & testV ==3)
+history.go(-1);
+return " ";
+}
